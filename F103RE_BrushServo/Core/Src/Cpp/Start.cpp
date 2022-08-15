71f170af754f5c514e00081259f8b8c0f0b0b5ff
Gabriel Lima <gabrielmartinslima@gmail.com>
2022-08-14 21:33:24 -0300

    ADC funcionando com testes

--- F103RE_BrushServo/Core/Src/Cpp/Start.cpp
+++ F103RE_BrushServo/Core/Src/Cpp/Start.cpp
@@ -11,17 +11,28 @@
 #include "BTS7960B.hpp"
 
 extern UART_HandleTypeDef huart1;
+extern ADC_HandleTypeDef hadc1;
 
 SerialDebug debug(&huart1);
 BTS7960B motor0(&(TIM3->CCR1), &(TIM3->CCR2), GPIOA, GPIO_PIN_4, GPIOA, GPIO_PIN_5);
+
+char adcprintbuf[64];
+uint32_t adcresult = 0;
 
 void HAL_UART_TxCpltCallback(UART_HandleTypeDef* huart){
 	debug.sendNext();
 }
 
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc){
+	sprintf(adcprintbuf, "ADC: %lu", adcresult);
+	debug.debug(adcprintbuf);
+}
+
 void Start(){
 	debug.setLevel(SerialDebug::DEBUG_LEVEL_DEBUG);
 	while(true){
+		HAL_ADC_Start_DMA(&hadc1, &adcresult, 1);
+		HAL_Delay(2);
 		debug.debug("512");
 		motor0.setSpeed(512);
 		HAL_Delay(500);
--- F103RE_BrushServo/Core/Src/main.c
+++ F103RE_BrushServo/Core/Src/main.c
@@ -40,6 +40,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
+DMA_HandleTypeDef hdma_adc1;
 
 TIM_HandleTypeDef htim3;
 TIM_HandleTypeDef htim6;
@@ -382,6 +383,9 @@ static void MX_DMA_Init(void)
   __HAL_RCC_DMA1_CLK_ENABLE();
 
   /* DMA interrupt init */
+  /* DMA1_Channel1_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
   /* DMA1_Channel4_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel4_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel4_IRQn);
